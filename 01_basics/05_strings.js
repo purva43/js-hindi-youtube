a2fe8631a59e686e50332ef04aaaec4a230d9bc6
purva43 <purvapawar0907@gmail.com>
2025-07-11 09:55:57 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,14 @@
+const name="purva";
+const repoCount="50"
+
+//console.log("name"+"50");
+//console.log(name + repoCount+ "value"); not use bcz it is outdated
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName=new String("purvap")
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a'));
