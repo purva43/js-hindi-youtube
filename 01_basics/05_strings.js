0303c5e2332b79c29d45eef518f9171d8e003355
purva43 <purvapawar0907@gmail.com>
2025-07-11 10:38:12 +0000

    strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -3,12 +3,32 @@ const repoCount="50"
 
 //console.log("name"+"50");
 //console.log(name + repoCount+ "value"); not use bcz it is outdated
-console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+//console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
 
-const gameName=new String("purvap")
-console.log(gameName[0]);
-console.log(gameName.__proto__);
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('a'));
+//const gameName=new String("purvap")
+//console.log(gameName[0]);
+//console.log(gameName.__proto__);
+//console.log(gameName.length);
+//console.log(gameName.toUpperCase());
+//console.log(gameName.charAt(2));
+//console.log(gameName.indexOf('a'));
+
+//const newString= gameName.substring(0,4)
+//console.log(newString);
+
+//const anotherString= gameName.slice(-5,2)
+//console.log(anotherString);//slice used to indicate negative value reverse value
+
+//const newString="  purva  "
+//console.log(newString);
+//console.log(newString.trim());
+
+//const url="https://purva.com/purva %20pawar";
+//console.log(url.replace('%20','-'))
+//console.log(url.includes("purva"))
+//console.log(url.includes("hitesh"))
+
+//want to covert string into arrays
+const gameName=new String("purva-vitthal-pawar")
+console.log(gameName.split('-'));
+console.log(gameName.split(""))
